5ab751f19e110df7063b7b38f43243a0b1cf895f
ahmed <amdade76@gmail.com>
2019-03-08 19:34:43 -0600

    fixed asg

--- asg.tf
+++ asg.tf
@@ -1,9 +1,9 @@
 resource "aws_autoscaling_group" "dev" {
     name = "dev"
     launch_configuration ="${aws_launch_configuration.dev.name}"
-    min_size = "{var.min_size}"
-    max_size = "{var.max_size}"
-    
+    min_size = "${var.min_size}"
+    max_size = "${var.max_size}"
+
 
 
 
